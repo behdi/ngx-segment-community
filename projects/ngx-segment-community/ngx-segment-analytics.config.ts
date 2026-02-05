13f7ae74ef4b459aef46b401443782f2ceca32e5
Behnam Sajadifar <beh.sajadi94@gmail.com>
2026-02-05 18:02:47 +0330

    feat: fork `ngx-segment-analytics` code

---
+++ projects/ngx-segment-community/ngx-segment-analytics.config.ts
@@ -0,0 +1,40 @@
+import { InjectionToken } from '@angular/core';
+import type { Plugin } from '@segment/analytics-next';
+
+/**
+ * Segment Configuration Interface
+ */
+export interface SegmentConfig {
+  /** API Key for Segment.io */
+  apiKey?: string;
+  /** Debug mode status */
+  debug?: boolean;
+  /** Load Segment configuration on initialization */
+  loadOnInitialization?: boolean;
+  /**
+   * Segment Host if requests are proxied
+   * @deprecated Use cdnURL instead.
+   **/
+  segmentHost?: string;
+  /**
+   *  Segment Custom URI
+   *  @deprecated Use cdnURL instead.
+   **/
+  segmentUri?: string;
+  /** CDN host if requests are proxied */
+  cdnURL?: string;
+  /** Plugins **/
+  plugins?: Plugin[];
+}
+
+/** Segment Configuration Injection Token */
+export const SEGMENT_CONFIG: InjectionToken<SegmentConfig> =
+  new InjectionToken<SegmentConfig>('ngx-segment-analytics.config');
+
+export const DEFAULT_CONFIG: SegmentConfig = {
+  debug: false,
+  loadOnInitialization: true, // Compatibility < 1.2.5
+  cdnURL: 'https://cdn.segment.com',
+  segmentUri: '/analytics.js/v1/$API_KEY$/analytics.min.js',
+  plugins: [],
+};
---
+++ projects/ngx-segment-community/window-wrapper.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+import type { AnalyticsSnippet } from '@segment/analytics-next';
+
+/**
+ * Window Wrapper for Angular AOT
+ */
+@Injectable()
+export class WindowWrapper {
+    /** Segment Analytics.js instance */
+    public analytics: AnalyticsSnippet;
+}
