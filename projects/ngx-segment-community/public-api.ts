0c597c79cb65dacf0568c73ce25b261b78448ff3
Behnam Sajadifar <beh.sajadi94@gmail.com>
2026-02-12 23:25:22 +0330

    feat: expose new provider and service from public api

--- projects/ngx-segment-community/public-api.ts
+++ projects/ngx-segment-community/public-api.ts
@@ -2,3 +2,10 @@ export * from './ngx-segment-analytics.config';
 export * from './ngx-segment-analytics.module';
 export * from './ngx-segment-analytics.service';
 export * from './window-wrapper';
+
+// Modern implementation
+export {
+  provideSegmentAnalytics,
+  type SegmentAnalyticsConfiguration,
+} from './provider';
+export { SegmentService as NewSegmentService } from './segment';
