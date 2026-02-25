c367c9b03743f9804fcb474faaa8f08fee6611d1
Behnam Sajadifar <beh.sajadi94@gmail.com>
2026-02-26 01:17:29 +0330

    feat: actually provide segment with writeKey safety

--- projects/playground/src/app/app.config.ts
+++ projects/playground/src/app/app.config.ts
@@ -4,12 +4,29 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideSegmentAnalytics,
+  withPlugins,
+  withSettings,
+} from 'ngx-segment-community';
+import { env } from '../environments';
 import { routes } from './app.routes';
+import { LiveFeedInterceptorPlugin } from './plugins';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideSegmentAnalytics(
+      withSettings({
+        // Create a file called `environment.local.ts`, add your writeKey there, and then serve
+        // the project in development mode
+        writeKey: env.SEGMENT_API_KEY,
+        initializationMode: 'manual',
+        debug: true,
+      }),
+      withPlugins([LiveFeedInterceptorPlugin]),
+    ),
   ],
 };
---
+++ projects/playground/src/environments/type.ts
@@ -0,0 +1,5 @@
+export interface Environment {
+  SEGMENT_API_KEY: string;
+}
+
+export type AlwaysEmptyString = '';
